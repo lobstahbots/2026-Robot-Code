73518c167825ba9c98c56fd1b41d67e648df91e1
Danesh Moghadaszadeh <danesh2020@icloud.com>
2026-01-27 15:17:30 -0500

    made some adjustments. Still needs to be fixed.

--- src/main/java/frc/robot/subsystems/Climber/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber/Climber.java
@@ -1,6 +1,8 @@
+package frc.robot.subsystems.Climber;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkMaxLowLevel.MotorType;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.config.SparkMaxConfig;
