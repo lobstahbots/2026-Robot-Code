81af347aa5849ad7c943ec8c73e8108e4c5d10ad
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2026-02-23 18:06:41 -0500

    add shotData command constructor & documentation update

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -12,6 +12,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.util.math.ShotData;
 
 public class Shooter extends SubsystemBase {
     private final ShooterIO io;
@@ -21,11 +22,21 @@ public class Shooter extends SubsystemBase {
         this.io = io;
     }
 
+    /**
+     * Get the hood position of this shooter.
+     * 
+     * @return The current hood position
+     */
     public Rotation2d getHoodPosition() {
         return inputs.hoodPosition;
     }
 
-    public AngularVelocity getHoodVelocity() {
+    /**
+     * Get the current flywheel velocity
+     * 
+     * @return The current flywheel velocity.
+     */
+    public AngularVelocity getFlywheelVelocity() {
         return inputs.hoodVelocity;
     }
 
@@ -35,14 +46,44 @@ public class Shooter extends SubsystemBase {
         Logger.processInputs("Shooter", inputs);
     }
 
+    /**
+     * Constructs a command which keeps the hood at an angle specified by a
+     * supplier.
+     * 
+     * @param angle A supplier for the angle, in the form of a {@link Rotation2d}.
+     * @return The constructed command.
+     */
     public Command hoodAngle(Supplier<Rotation2d> angle) {
         return run(() -> io.setHoodPosition(angle.get()));
     }
 
+    /**
+     * Constructs a command which keeps the hood at an angle specified by a supplier
+     * and the flywheel at a velocity specified by a supplier.
+     * 
+     * @param hoodAngle        A supplier for the hood angle as a
+     *                         {@link Rotation2d}.
+     * @param flywheelVelocity A supplier for the rotational velocity of the
+     *                         flywheel as a {@link AngularVelocity} Measure object.
+     * @return The constructed command.
+     */
     public Command operate(Supplier<Rotation2d> hoodAngle, Supplier<AngularVelocity> flywheelVelocity) {
         return run(() -> {
             io.setHoodPosition(hoodAngle.get());
             io.setFlywheelVelocity(flywheelVelocity.get());
         });
+    }
+
+    /**
+     * Constructs a command which keeps the hood at an angle and the flywheel at a
+     * velocity specified by a supplier.
+     * 
+     * @param shotData The supplier for the data for the shot, which supplies
+     *                 {@link ShotData} objects.
+     * @return The constructed command.
+     * @see ShotData
+     */
+    public Command operate(Supplier<ShotData> shotData) {
+        return operate(() -> shotData.get().hoodPosition(), () -> shotData.get().flywheelVelocity());
     }
 }
