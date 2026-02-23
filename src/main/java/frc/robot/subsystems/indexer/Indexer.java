b699c663eeb3024e4b7da2a62ade993babae6b34
ChiliRhino417 <100389731+shi-teddy@users.noreply.github.com>
2026-02-23 12:45:02 -0500

    ak + commands

--- src/main/java/frc/robot/subsystems/indexer/Indexer.java
+++ src/main/java/frc/robot/subsystems/indexer/Indexer.java
@@ -4,50 +4,44 @@
 
 package frc.robot.subsystems.indexer;
 
+import org.littletonrobotics.junction.Logger;
 
-import com.revrobotics.PersistMode;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.ResetMode;
-import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.config.SparkMaxConfig;
-import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.IndexerConstants;
 
 public class Indexer extends SubsystemBase {
-
-    private final SparkMax indexerMotor;
-    private final RelativeEncoder encoder;
-    /** Creates a new Indexer. */
-    public Indexer(int indexerMotorID) {
-        this.indexerMotor = new SparkMax(indexerMotorID, MotorType.kBrushless);
     
-        SparkMaxConfig config = new SparkMaxConfig();
-        config.smartCurrentLimit(IndexerConstants.CURRENT_LIMIT).idleMode(IdleMode.kBrake).inverted(false).encoder.velocityConversionFactor(3);
+    private final IndexerIO io;
+    private final IndexerIOInputsAutoLogged inputs = new IndexerIOInputsAutoLogged();
 
-        indexerMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
-
-        encoder = indexerMotor.getEncoder();
+    /** Creates a new Indexer. */
+    public Indexer(IndexerIO io) {
+        this.io = io;
     }
 
     public void setIndexerSpeed(double speed) {
-        indexerMotor.set(speed);
+        io.setIndexerSpeed(speed);
     }
 
-    public void stopIndexer() {
-        indexerMotor.stopMotor();
+    public void stopIndexerMotor() {
+        io.stopIndexer();
     }
 
     public void setIdleMode(boolean isBrake) {
-        SparkMaxConfig config = new SparkMaxConfig();
-        config.idleMode(isBrake ? IdleMode.kBrake : IdleMode.kCoast);
-        indexerMotor.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        io.setIdleMode(isBrake);
+    }
+
+    public double getIndexerSpeed() {
+        return inputs.indexerVelocity;
     }
 
     @Override
     public void periodic() {
-        // This method will be called once per scheduler run
+        io.updateInputs(inputs);
+        Logger.processInputs("Indexer", inputs);
+    }
+
+    public Command spin() {
+        return runEnd(() -> io.setIndexerSpeed(1), io::stopIndexer);
     }
 }
